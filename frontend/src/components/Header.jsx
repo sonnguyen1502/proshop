b78b14fb0df9c59e53b52f31241a649e6f126f0a
Nguyen Huu Son <lohocsinhngoan@gmail.com>
2024-03-25 10:32:19 +0700

    Create List Products

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
             <Container>
                 <Navbar.Brand href="/">
                     <img src={logo} alt="P-TechShop"/>
-                    -TechShop
+                    TechShop
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
